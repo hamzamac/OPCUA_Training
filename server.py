c57db506e1e7afb2cb6c2f244edc86676ec1c802
Makame Makame <hamzamac@live.com>
2018-09-17 16:35:21 +0200

    Added a objecttype node

--- server.py
+++ server.py
@@ -8,10 +8,21 @@ def main():
     server = Server()
     server.set_endpoint("opc.tcp://0.0.0.0:4040/simumatik")
     server.set_server_name("Simumatik OpcUa Server")
-    server.set_security_policy(ua.SecurityPolicyType.NoSecurity)
+    #server.set_security_policy(ua.SecurityPolicyType.NoSecurity)
 
-    logging.info("Server started")
 
+    #create node
+    node1 = server.nodes.base_object_type.add_object_type(0, "Cube")
+    node1.add_variable(0, "height", 3.0).set_modelling_rule(True)
+    node1.add_variable(0, "width", 3.0).set_modelling_rule(True)
+    node1.add_variable(0, "bredth", 3.0).set_modelling_rule(True)
+    node1.add_property(0, "cube_id", "1001")
+
+    #controller = node1.add_object(0, "controller")
+    #controller.set_modelling_rule(True)
+    #controller.add_property(0, "state", "Visible").set_modelling_rule(True)
+
+    server.start()
 
 
 if __name__ == "__main__":
