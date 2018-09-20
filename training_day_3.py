ebf5eec042678693a4c8610525e19815a7fb390c
Makame Makame <hamzamac@live.com>
2018-09-20 12:25:48 +0200

    export a type object

--- training_day_3.py
+++ training_day_3.py
@@ -1,4 +1,5 @@
-from opcua import Server, ua, instantiate
+from opcua import Server, ua, uamethod
+from opcua.common.xmlexporter import XmlExporter
 
 def main():
     server = Server()
@@ -11,10 +12,22 @@ def main():
     motor_object_type.add_variable(0, "speed", 0, varianttype=ua.VariantType.Int32).set_modelling_rule(True)
     motor_object_type.add_property(0, "name", "IndustialMotor", varianttype=ua.VariantType.String).set_modelling_rule(True)
    
-    motor_object = instantiate(server.nodes.objects, motor_object_type,bname="0:namee")
-    #motor_object = server.nodes.objects.add_object(address_space, "RefenaryMotor", objecttype=motor_object_type)        
+    #motor_object = instantiate(server.nodes.objects, motor_object_type,bname="0:namee")
+    motor_object = server.nodes.objects.add_object(address_space, "RefinaryMotor", objecttype=motor_object_type)        
+
+    #Lering Node object functions
+    #print(motor_object.get_browse_name()) #QualifiedName(2:RefinaryMotor)
+    #print(motor_object.get_node_class())
+    #print(server)
+
+    #expoting Method
 
     server.start()
+
+    #XML
+    xml_exporter = XmlExporter(server)
+    xml_exporter.build_etree([motor_object_type])
+    xml_exporter.write_xml("motor_object_type.xml")
 
 if __name__ == "__main__" :
     main()
